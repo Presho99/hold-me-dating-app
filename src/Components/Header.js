7a5c06fab26ecde65e7ad53239e82b171b716ad4
Precious <Preshomondi@gmail.com>
2022-09-11 11:59:47 +0300

    Create and style header component

---
+++ src/Components/Header.js
@@ -0,0 +1,29 @@
+import React from "react"
+import './Header.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHandHoldingHeart, faSearch, faMessage, faAddressBook, faCircleInfo, faGear, faGlobe, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="logo">
+                <h3>HoldMe<FontAwesomeIcon icon={faHandHoldingHeart} style={{ marginLeft: "10" }} /></h3>
+            </div>
+
+            <div className="search">
+                <form className="searchbar">
+                    <input type="text" placeholder="Search user" />
+                    <button><FontAwesomeIcon icon={faSearch} /></button>
+                </form>
+            </div>
+
+            <div className="header-profile">
+                <p>Hi, Manuel</p>
+                <img src='profile.webp'></img>
+            </div>
+        </div>
+    )
+}
+
+export default Header
