7c00dc3eb003e72127037266d2b1ff1af76b9ff6
Precious <Preshomondi@gmail.com>
2022-09-12 09:11:36 +0300

    Control forms in Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -6,7 +6,7 @@ import "./Signup.css"
 
 function Signup(){
     let history = useHistory()
-    const [signupDetails,setSignupDetails]=useState('');
+    const [signupDetails,setSignupDetails]=useState({first_name:"", password:"", confirm_password:"", location: "", job: "", birth: "", gender: "" });
 
     const handleChange = (e) => {
         setSignupDetails(e.target.value)
@@ -18,13 +18,13 @@ function Signup(){
             <h1><FontAwesomeIcon icon={faHandHoldingHeart} style={{fontSize: "80", color: "#9D2E3C" }}/></h1>
                 <form>
                     <div className="form-name">
-                    <input type="text" placeholder='First name' style={{width:"400px"}}></input>
+                    <input type="text" placeholder='First name' style={{width:"400px"}} value={signupDetails.first_name} name="first_name"></input>
                     
                     </div>
 
                     <div className="form-password">
-                    <input type="password" placeholder='Password' required></input>
-                    <input type="password" placeholder='Confirm Password' style={{marginLeft: "10px"}} required></input>
+                    <input type="password" placeholder='Password' required value={signupDetails.password} name={password}></input>
+                    <input type="password" placeholder='Confirm Password' style={{marginLeft: "10px"}} required value={signupDetails.confirm_password} name={confirm_password}></input>
 
                     </div>
                     </form>
@@ -32,19 +32,19 @@ function Signup(){
                     <form>
                     
                     <div className="loc-job">
-                    <input type="text" placeholder='Location' required></input>
-                    <input type="text" placeholder='Field of Work' style={{marginLeft: "20px"}}></input>
+                    <input type="text" placeholder='Location' required value={signupDetails.location} name={location}></input>
+                    <input type="text" placeholder='Field of Work' style={{marginLeft: "20px"}} value={signupDetails.job} name={job}></input>
                     </div>
                     <div className="birth-gender">
                     <label style={{color: "grey", fontSize: "14px"}}>DOB:
-                    <input type="date"/>
+                    <input type="date" value={signupDetails.birth} name={birth}/>
                     </label>
 
-                    <input type="radio" value="male" id="male"
+                    <input type="radio" value={signupDetails.gender} id="male"
                onChange={handleChange} name="gender" style={{backgroundColor: "pink"}}/>
              <label for="male" style={{color: "grey"}}>Male</label>
 
-            <input type="radio" value="female" id="female"
+            <input type="radio" value={signupDetails.gender} id="female"
               onChange={handleChange} name="gender"/>
             <label for="female" style={{color: "grey"}}>Female</label>
                     </div>
